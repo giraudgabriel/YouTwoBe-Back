5de724686e39c09ecc3dce0f8efba43b49a141b5
Gabriel Fernandes Giraud <gabriel.giraud@fatec.sp.gov.br>
2021-07-04 21:55:19 -0300

    Works aqui

--- index.js
+++ index.js
@@ -1,38 +1,32 @@
 const app = require("express")();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
+const cors = require("cors");
 const { v4: uuidv4 } = require("uuid");
+
+app.use(cors());
+
 //const _ = require("lodash");
 
 //#region Variables of control
-let users = [];
-let rooms = [];
-//#endregion
-
-io.on("connection", onConnect);
-
-server.listen(3000, () => {
-  console.log("TO ON PAE");
-});
-
-//#region Connect
-const onConnect = (socket) => {
-  const { id } = socket.client.conn;
-  console.log(`${id} conectou-se ao socket`);
-
-  socket.on("disconnect", () => onDisconnect(id));
-  socket.on("joinRoom", (data) => onJoinRoom(data, id, socket));
-  socket.on("createRoom", (data) => onCreateRoom(data, id, socket));
-  socket.on("playVideo", (data) => onPlayVideo(data, id, socket));
-  socket.on("pauseVideo", (data) => onPauseVideo(data, id, socket));
-};
-
+let users = {};
+let rooms = {};
 //#endregion
 
 //#region Disconnect
-const onDisconnect = (id) => {
+const onDisconnect = (id, socket) => {
   console.log(`${id} desconectou-se`);
+  tryLeaveRoom(id);
+  socket.removeAllListeners();
 };
+
+const tryLeaveRoom = (userId) => {
+  Object.keys(rooms, (key) => {
+    if (rooms[key].connections.some((id) => id == userId))
+      rooms[key].connections = rooms[key].filter((id) => id == userId);
+  });
+};
+
 //#endregion
 
 //#region Chat
@@ -58,7 +52,7 @@ const onCreateRoom = (data, userId, socket) => {
   rooms[id] = newRoom;
 
   socket.join(id);
-  onSendMessageToRoom(id, `${name} entrou na sala`);
+  onSendMessageToRoom(socket, id, `${name} entrou na sala`);
 };
 
 const onJoinRoom = (data, id, socket) => {
@@ -92,3 +86,22 @@ const onPauseVideo = (data, userId, socket) => {
 };
 
 //#endregion
+
+//#region Connect
+const onConnect = (socket) => {
+  const { id } = socket.client.conn;
+  console.log(`${id} conectou-se ao socket`);
+  socket.on("disconnect", () => onDisconnect(id, socket));
+  socket.on("joinRoom", (data) => onJoinRoom(data, id, socket));
+  socket.on("createRoom", (data) => onCreateRoom(data, id, socket));
+  socket.on("playVideo", (data) => onPlayVideo(data, id, socket));
+  socket.on("pauseVideo", (data) => onPauseVideo(data, id, socket));
+};
+
+//#endregion
+
+io.on("connection", onConnect);
+
+server.listen(3001, () => {
+  console.log("TO ON PAE");
+});
